24fa2d056c46b16b78e68a13eacb279df0c4958f
kevinokello <okellokevin77@gmail.com>
2023-04-18 02:48:20 +0300

    tasks and user tasks crud insomnia requests

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Resources\TaskResource;
+use App\Http\Resources\TaskCollection;
+use App\Http\Requests\StoreTaskRequest;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        // return TaskResource::collection(Task::all());
+        return new TaskCollection(Task::all());
+    }
+    public function show(Task $task)
+    {
+        return new TaskResource($task);
+    }
+    public function store(StoreTaskRequest $request)
+    {
+        Task::create($request->validated());
+        return response()->json("Task created");
+    }
+
+    public function update(StoreTaskRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+        return response()->json("Task created");
+    }
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json("Task deleted");
+
+    }
 }
--- backend/app/Http/Controllers/UserTaskController.php
+++ backend/app/Http/Controllers/UserTaskController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserTask;
 use Illuminate\Http\Request;
+use App\Http\Resources\UserTaskResource;
+use App\Http\Resources\UserTaskCollection;
+use App\Http\Requests\StoreUserTaskRequest;
 
 class UserTaskController extends Controller
 {
-    //
+    public function index()
+    {
+        // return TaskResource::collection(Task::all());
+        return new UserTaskCollection(UserTask::all());
+    }
+    public function show(UserTask $usertask)
+    {
+        return new UserTaskResource($usertask);
+    }
+    public function store(StoreUserTaskRequest $request)
+    {
+        UserTask::create($request->validated());
+        return response()->json("Task created");
+    }
+
+    public function update(StoreUserTaskRequest $request, UserTask $usertask)
+    {
+        $usertask->update($request->validated());
+        return response()->json("Task created");
+    }
+    public function destroy(UserTask $usertask)
+    {
+        $usertask->delete();
+        return response()->json("Task deleted");
+
+    }
 }
